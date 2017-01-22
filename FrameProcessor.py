1f4a760b8ee063c17417d29188c665c9e110195d
Aleksey <alex.churshin@gmail.com>
2017-01-22 23:50:39 +0300

    Changed extracting of convex defects to better approximate number of fingers. Added collections for tracking latest hand points and finger count.

--- FrameProcessor.py
+++ FrameProcessor.py
@@ -1,52 +1,57 @@
 import sys
 import numpy as np
+import pyautogui as pag
 import cv2
 
-from collection import deque
+from collections import deque
 
+
+MIN_CONVEX_HULL_LENGTH = 200
 
 class FrameProcessor:
+
 
 	def __init__(self):
 		self.cap = cv2.VideoCapture(0)
 		if not self.cap.isOpened():
 			raise Exception("Can't access web-camera")
-		self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1000)
-		self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 600)
-		self.hand_centers = deque(maxlen=100)
+		self.screen_size = pag.size()
+		self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.screen_size[0])
+		self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.screen_size[1])
+		self.hand_centers = deque(maxlen=100) # collection for storing last 100 hand positions
+		self.fingers = deque(maxlen=100) # collection for storing very approximate number of fingers 
+		self.recent_click = False # trace whether clicks were made recently
 
 	def get_next_frame(self):
 		_, self.frame = self.cap.read()
 
+	# couldn't properly implement, commented for later fixes
+	# def erose_and_dilate(self):
+	# 	self.hsv = cv2.cvtColor(self.frame, cv2.COLOR_BGR2HSV)
+	# 	mask = cv2.inRange(hsv, np.array([2, 50, 50]), np.array([15, 255, 255]))
 
-	def erose_and_dilate(self):
-		self.hsv = cv2.cvtColor(self.frame, cv2.COLOR_BGR2HSV)
-		mask = cv2.inRange(hsv, np.array([2, 50, 50]), np.array([15, 255, 255]))
-
-		dilation = cv2.dilate(mask2, kernel_ellipse, iterations=1)
-		erosion = cv2.erode(dilation, kernel_square, iterations=1)    
-		dilation2 = cv2.dilate(erosion, kernel_ellipse, iterations=1)    
-		filtered = cv2.medianBlur(dilation2, 5)
-		kernel_ellipse = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (8, 8))
-		dilation2 = cv2.dilate(filtered, kernel_ellipse,iterations=1)
-		kernel_ellipse= cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
-		dilation3 = cv2.dilate(filtered,kernel_ellipse, iterations=1)
-		median = cv2.medianBlur(dilation2, 5)
-		_, self.threshold2 = cv2.threshold(median, 127, 255, 0)
+	# 	dilation = cv2.dilate(mask2, kernel_ellipse, iterations=1)
+	# 	erosion = cv2.erode(dilation, kernel_square, iterations=1)    
+	# 	dilation2 = cv2.dilate(erosion, kernel_ellipse, iterations=1)    
+	# 	filtered = cv2.medianBlur(dilation2, 5)
+	# 	kernel_ellipse = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (8, 8))
+	# 	dilation2 = cv2.dilate(filtered, kernel_ellipse,iterations=1)
+	# 	kernel_ellipse= cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+	# 	dilation3 = cv2.dilate(filtered,kernel_ellipse, iterations=1)
+	# 	median = cv2.medianBlur(dilation2, 5)
+	# 	_, self.threshold2 = cv2.threshold(median, 127, 255, 0)
 
 	
 	def get_threshold(self):
-		
-
 		gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
-		blurred = cv2.GaussianBlur(gray, (23, 23), 0)
+		blurred = cv2.GaussianBlur(gray, (23, 23), 0) #blurring image to smooth acute borders to ease thresholding
 		_, self.threshold = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
-
+	# Work on the assumption that the hand is closer to the camera and so it occupies the biggest area of the screen.
 	def get_hand_contour(self):
 		_, self.contours, __ = cv2.findContours(self.threshold.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 		maxArea, cont = 0, None
-		for i, c in enumerate(self.contours):
+		for c in self.contours:
 			area = cv2.contourArea(c)
 			if area > maxArea:
 				maxArea = area
@@ -58,8 +63,6 @@ class FrameProcessor:
 		self.hand_x, self.hand_y, self,hand_width, self.hand_height = cv2.boundingRect(self.hand_contour)
 
 
-
-
 	def get_hulls(self):
 		self.hull_contour = cv2.convexHull(self.hand_contour, returnPoints=False)
 		self.hull_points = [self.hand_contour[i[0]] for i in self.hull_contour]
@@ -67,7 +70,16 @@ class FrameProcessor:
 
 
 	def get_defects(self):
-		self.defects = cv2.convexityDefects(self.handContour, self.hullHandContour) 
+		self.defects = cv2.convexityDefects(self.hand_contour, self.hull_contour) 
+		number_of_defects = 0
+		for defect in self.defects:
+			start, end, farthest_index, distance = defect[0]
+			if distance >= MIN_CONVEX_HULL_LENGTH:
+				number_of_defects += 1
+		self.fingers.append(number_of_defects)
+		print("defects")
+		print(self.defects)
+		print(self.fingers)
 
 
 	def get_center(self):
@@ -75,3 +87,4 @@ class FrameProcessor:
 		mom_x = int(moments['m10'] / moments['m00'])
 		mom_y = int(moments['m01'] / moments['m00'])
 		self.hand_centers.append((mom_x, mom_y))
+
