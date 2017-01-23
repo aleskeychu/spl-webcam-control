76e5f9bec6453b1ddeb0703f69d236d82384dfaf
Aleksey <alex.churshin@gmail.com>
2017-01-23 19:19:45 +0300

    Added primary loop, main function, tweaked hand movements to make them smoother.

--- FrameProcessor.py
+++ FrameProcessor.py
@@ -2,10 +2,12 @@ import sys
 import numpy as np
 import pyautogui as pag
 import cv2
+import argparse
+import math
 
 from collections import deque
 
-
+pag.FAILSAFE = False
 MIN_CONVEX_HULL_LENGTH = 100
 
 class FrameProcessor:
@@ -16,10 +18,10 @@ class FrameProcessor:
 		if not self.cap.isOpened():
 			raise Exception("Can't access web-camera")
 		self.screen_size = pag.size()
-		self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.screen_size[0])
-		self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.screen_size[1])
+		self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.screen_size[1])
+		self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.screen_size[0])
 		self.hand_centers = deque(maxlen=15) # collectionq for storing last 100 hand positions, tuples of format (x, y)
-		self.fingers = deque(maxlen=15) # collection for storing very approximate number of fingers
+		self.fingers = deque(maxlen=10) # collection for storing very approximate number of fingers
 		self.recent_click = False # trace whether clicks were made recently
 
 	def get_next_frame(self):
@@ -64,9 +66,9 @@ class FrameProcessor:
 		self.hand_x, self.hand_y, self,hand_width, self.hand_height = cv2.boundingRect(self.hand_contour)
 
 
-	def get_hulls(seqlf):
-		self.hull_contour = cv2.convexHull(self.hand_contour, returnPoints=False)
-		self.hull_points = [self.hand_contour[i[0]] for i in self.hull_contour]
+	def get_hulls(self):
+		self.hull_contour = cv2.convexHull(self.hand_contour, returnPoints=False) # returns indeces
+		self.hull_points = [self.hand_contour[i[0]] for i in self.hull_contour] # actual points
 		self.hull_points = np.array(self.hull_points, dtype=np.int32)
 
 
@@ -92,14 +94,52 @@ class FrameProcessor:
 		if len(self.hand_centers) < 2:
 			return
 		dots = self.hand_centers
-		delta = ((dots[-1][0] - dots[-2][0]) * 2, (dots[-1][1] - dots[-2][1]) * 2)
-		pag.moveRel(*delta)
+		delta = ((dots[-1][0] - dots[-2][0]) * 2, (dots[-1][1] - dots[-2][1]) * 2) # calculatuing distance between 2 latest hand positions
+		if math.sqrt(delta[0]**2 + delta[1]**2) < (self.screen_size[0] / 10): # filtering big distances as they are probably algorithm errors
+			pag.moveRel(*delta)
 
 
 	def register_click(self):
 		delta = self.fingers[-1] - self.fingers[0]
-		if delta < -3 and not self.recent_click:
+		# print(delta)
+		if delta < -3 and not self.recent_click: # 3 is an arbitrary number of fingers found empiricaly
 			self.recent_click = True
 			pag.click()
-		elif delta > 3:
+			# print('click Done')
+		elif delta > 3 and self.recent_click:
+			# print('click Undone')
 			self.recent_click = False
+
+
+	def run(self):
+		fp.get_next_frame()
+		fp.get_threshold()
+		fp.get_hand_contour()
+		fp.get_hulls()
+		fp.get_center()
+		fp.get_defects()
+		fp.move()
+		fp.register_click()
+
+
+
+
+if __name__ == "__main__":
+	
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--show-video", help="also play video from the webcam with various filters", action="store_true")
+	args = parser.parse_args()
+
+	fp = FrameProcessor()
+	while True:
+		fp.run()
+		if args.show_video:
+			img = np.zeros((fp.frame.shape[0], fp.frame.shape[1] * 2), np.uint8)
+			cv2.drawContours(img, fp.hand_contour, -1, (0, 255, 0), 3)
+			cv2.circle(img, fp.hand_centers[-1], 10, (255, 0, 0))
+			roi = img[:,fp.frame.shape[1]:]
+			img[:,fp.frame.shape[1]:] = fp.threshold
+			cv2.imshow('frame', img)
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+			break
+	cv2.destroyAllWindows()
